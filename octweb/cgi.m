a1c8cec5492259f62db2d9a38fba7d349c059bfe
Zach Ploskey <zploskey@gmail.com>
2014-04-02 14:37:42 -0700

    some progress, now calls into functions and prints things

---
+++ octweb/cgi.m
@@ -0,0 +1,79 @@
+%% -*- texinfo -*-
+%% @deftypefn {Function File} {@var{CGI} =} cgi ()
+%% Creates a CGI object to parse CGI query string from GET or POST requests.
+%% Parameter can be return by the functions getfirst and getlist or
+%% with the dot-syntax (CGI.form.name).
+%%
+%% The methods getfirst and getlist behave as the Python CGI functions.
+%% 
+%% @end deftypefn
+%% @seealso{@@cgi/getfirst,@@cgi/getlist}
+
+
+function retval = cgi()
+
+retval.request_method = getenv('REQUEST_METHOD');
+
+retval.params = {};
+retval.vals = {};
+
+if strcmp(retval.request_method,'GET') || ...
+   strcmp(retval.request_method,'HEAD')
+  % GET/HEAD request
+  retval.query_string = getenv('QUERY_STRING');
+elseif strcmp(retval.request_method,'POST')
+  % POST request
+  content_type = getenv('CONTENT_TYPE');
+  content_length = str2double(getenv('CONTENT_LENGTH'));
+  assert(content_type,'application/x-www-form-urlencoded');
+  retval.query_string = fscanf(stdin,'%c',content_length);
+  %fprintf(stderr,'query_string "%s" "%s" "%d"',retval.query_string,content_type,content_length);
+else
+  error('unsupported requested method',retval.request_method);
+end
+
+
+% should also split at ";"
+p = strsplit(retval.query_string,'&');
+
+for i=1:length(p)
+  pp = strsplit(p{i},'=');
+  
+  retval.params{end+1} = unquote(pp{1});
+  retval.vals{end+1} = unquote(pp{2});
+end
+
+% replace strings like 'abc%20def' to 'abc def'
+function uq = unquote(s)
+
+% replace + by space
+s = strrep(s,'+',' ');
+
+% decode percent sign + hex value
+uq = '';
+i = 1;
+while i <= length(s)
+  if strcmp(s(i),'%')
+    uq = [uq char(hex2dec(s(i+1:i+2)))];
+    i = i+3;
+  else
+    uq = [uq s(i)];
+    i = i+1;
+  end
+end
+
+
+% Copyright (C) 2012 Alexander Barth <barth.alexander@gmail.com>
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; If not, see <http://www.gnu.org/licenses/>.
---
+++ octweb/get_mlmdir.m
@@ -0,0 +1,31 @@
+function code_dir = get_mlmdir(fd)
+
+# read lines until we find the directory with source code (mldir)
+match = false;
+while (~match)
+	ln = fgetl(fd);
+
+	if (ln == -1)
+		printf("Invalid configuration file");
+		fclose(fd);
+		quit();
+	end
+
+	#are we on the mldir line
+	if (regexp(ln, '^mldir='))
+		# split on the equals sign
+		split_ln = strsplit(ln, '=');
+		# set the return value
+		code_dir = split_ln(2){1};
+		# fail if the directory doesn't exist
+		if (~exist(code_dir, 'dir'))
+			printf("%s file does not exist", code_dir);
+			fclose(fd);
+			quit();
+		end
+
+		match = true;
+	end
+end
+
+end %function
